61e734090c89742aeab48cd1be09451ddbdf497d
Cameron Cundiff and Trevor John <pair+ccundiff+tjohn@pivotallabs.com>
2013-07-18 09:45:16 -0400

    WIP

---
+++ test/js/audit-results-test.js
@@ -0,0 +1,14 @@
+module("AuditResults", {
+  setup: function () {
+    var input = document.createElement('input');
+    input.setAttribute('id', 'no-label');
+    input.type = 'text';
+    var fixture = document.getElementById("fixture");
+    fixture.appendChild(
+
+  }
+});
+
+it("#getErrors (default)", function () {
+  debugger;
+});
